74c7cd7f562337843b4ef1ce28620e66077902b4
tywaunburks-dot <tywaunburks@gmail.com>
2026-01-06 06:42:52 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+(function () {
+  const toggle = document.querySelector('[data-mobile-toggle]');
+  const menu = document.querySelector('[data-nav-links]');
+  if (toggle && menu) {
+    toggle.addEventListener('click', () => {
+      menu.classList.toggle('show');
+    });
+  }
+
+  const y = document.querySelector('[data-year]');
+  if (y) y.textContent = new Date().getFullYear();
+})();
